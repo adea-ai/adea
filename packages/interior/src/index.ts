44a5f8f1599304d4bbc64f6ae38e8862b8b04c49
NiftyAndy <andy@niftyleague.com>
2026-08-22 13:53:43 -0400

    perf(web): isolate package validation schemas

--- packages/interior/src/index.ts
+++ packages/interior/src/index.ts
@@ -3,5 +3,3 @@ export * from "./runtime";
 export * from "./room-config";
 
 export { interiorPropAssets, propAssets } from "./catalog";
-export { interiorPropAssetSchema, interiorPropCatalogSchema } from "./schemas";
-export type { ValidatedInteriorPropAsset } from "./schemas";
